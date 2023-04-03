cc6553933c2dd5607070253fd1c8cc94ce5209fb
Aziz Solihin <azizslhn45@gmail.com>
2023-04-03 15:56:34 +0700

    update config

--- src/config.js
+++ src/config.js
@@ -1,10 +1,9 @@
 export const PORT = process.env.PORT || 5000;
 
-export const DB_HOST =
-  process.env.MYSQLHOST || "containers-us-west-185.railway.app";
-export const DB_USER = process.env.MYSQLUSER || "root";
-export const DB_PASSWORD = process.env.MYSQLPASSWORD || "j1kwv9GUEC5ugHsEhELi";
-export const DB_NAME = process.env.MYSQLDATABASE || "railway";
-export const DB_PORT = process.env.MYSQLPORT || 6178;
+export const DB_HOST = process.env.MYSQLHOST;
+export const DB_USER = process.env.MYSQLUSER;
+export const DB_PASSWORD = process.env.MYSQLPASSWORD;
+export const DB_NAME = process.env.MYSQLDATABASE;
+export const DB_PORT = process.env.MYSQLPORT;
 
-export const SECRET_KEY = process.env.SECRETKEY || "12fn1iencunAnW&naUP!Nua1";
+export const SECRET_KEY = process.env.SECRETKEY;
