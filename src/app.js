d8d00e27a26e19e1a49a5571112a047b4501e48d
Aziz Solihin <azizslhn45@gmail.com>
2023-04-03 15:22:33 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 import express from "express";
+import { pool } from "./db.js";
+import { PORT } from "./config.js";
 
 const app = express();
 
 app.get("/", async (req, res) => {
-  // const [rows] = await
-  res.json("Developing");
+  res.json({ payload: "Selamat Datang" });
 });
 
-app.listen(3000, () => {
+app.get("/bank-soal", async (req, res) => {
+  const [fields] = await pool.query(`SELECT * FROM bank`);
+
+  res.json({ payload: "Bank Soal Kepramukaan", data: fields });
+});
+
+app.listen(PORT, () => {
   console.log("Server running...");
 });
