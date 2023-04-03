3c040d59cdfccd476b94e9dba2c80a97a7524902
Aziz Solihin <azizslhn45@gmail.com>
2023-04-03 15:29:24 +0700

    clear

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 export const PORT = process.env.PORT || 5000;
 
-export const DB_HOST =
-  process.env.MYSQLHOST;
+export const DB_HOST = process.env.MYSQLHOST;
 export const DB_USER = process.env.MYSQLUSER;
 export const DB_PASSWORD = process.env.MYSQLPASSWORD;
 export const DB_NAME = process.env.MYSQLDATABASE;
